8770a39fc7a53d8efc6a514647baf4a1d21284ab
jun nishii <jun@tutti.jp>
2018-10-02 09:42:20 +0900

    some updates

--- docker-run.sh
+++ docker-run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-DKNAME=opencv3
+DKNAME=jupyter
 DKUSER=jovyan
 DKHOME=/home/${DKUSER}
 PWD=`pwd`
 WD="${PWD}"
-IMAGE=jnishii/docker-anaconda3-opencv3
+IMAGE=jnishii/docker-gym-nongpu
 
 DKOPT="--rm \
 	-h ${DKNAME} \
@@ -22,10 +22,19 @@ DKOPT="--rm \
 #
 # https://docs.docker.com/engine/reference/commandline/run/#examples
 
+Usage(){
+	echo "docker-run.sh [option]"
+	echo "  -h    : show this usage"
+	echo "  -X    : start bash terminal and X server"
+	echo "  -bg   : start jupyter notebook in background mode"
+	echo "  -fg (default) : start jupyter notebook in foreground mode"
+}
+
 runX(){
 	IP=`ipconfig getifaddr en0`
 
 	echo "running X server and bash ..."
+	echo "You can start jupyter by typing jupyter.sh"
 	echo ""
 	echo "If you want to connet X server on Docker, run X client on your system."
 	echo "  On Mac, follow the instruction below before running this script."
@@ -39,10 +48,29 @@ runX(){
 		${DKOPT} \
 		-e XMODIFIERS \
 		-e DISPLAY=${IP}:0 \
-		-v /tmp/.X11-unix:/tmp/.X11-unix \
+		-v /tmp/.X11-unix:/tmp/.X11-unix:rw --privileged \
 		${IMAGE} /bin/bash
 
 	xhost -
 }
 
-runX
+runJupyterBG(){
+	echo "running docker in background mode..."
+	echo 
+	echo "$ docker logs ${DKNAME}  # confirm docker token"
+	echo "$ docker stop ${DKNAME}  # stop docker" 
+	echo "$ docker start ${DKNAME} # start docker" 
+
+	docker run -d ${DKOPT} ${IMAGE}
+}
+
+runJupyterFG(){
+	docker run -it ${DKOPT} ${IMAGE}
+}
+
+case ${1} in
+	-h)  Usage ;;
+    -X)  runX ;;
+    -bg) runJupyterBG ;;
+    *) runJupyterFG ;;
+esac
